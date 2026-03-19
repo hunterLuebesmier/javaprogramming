d1adc687fff077f700c1972aa98de5e25d6ab2af
hunterLuebesmier <hunterluebesmier69@gmail.com>
2026-03-19 16:04:32 +0000

    3 more challanges

---
+++ NotExample.java
@@ -0,0 +1,12 @@
+public class NotExample {
+    public static void main(String[] args) {
+
+        boolean isRaining = false;
+
+        if (!isRaining) {
+            System.out.println("You don't need an umbrella.");
+        } else {
+            System.out.println("Bring an umbrella.");
+        }
+    }
+}
---
+++ TeenChecker.java
@@ -0,0 +1,12 @@
+public class TeenChecker {
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if (age >= 13 && age <= 19) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are not a teenager.");
+        }
+    }
+}
